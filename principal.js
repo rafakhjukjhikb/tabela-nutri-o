91875b236a1a8cc4af03be364086465bf59d6e23
rafakhjukjhikb <rafaela.castro.moura@escola.pr.gov.br>
2025-09-04 09:00:10 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,12 +1,14 @@
-//muda o nome a partir do seletor de classe
-var pacientes = document.querySelector(".subtitulo");
-			pacientes.textContent = "Meus Pacientes";
-			var nutrição = document.querySelector(".titulo");
-			nutrição.textContent = "Rafa Nutrição";
-//acessara tag tr
-            var paciente = document.querySelector("#primeiro-paciente");
-            console.log(paciente);
-            //seleciona o conteudo
+            //muda o nome a partir do seletor de classe
+            var subtitulo= document.querySelector(".subtitulo");
+			var titulo = document.querySelector(".titulo");
+			titulo.textContent = "Rafa Nutrição";
+            subtitulo.textContent = "meus pacientes";
+
+            //acessara tag tr - paciente Paulo
+            var paciente = document.querySelector(".primeiro");
+           
+            for(var i = 0; i < paciente.length; i++){
+                 //seleciona o conteudo
             var tdpeso =paciente.querySelector(".info-peso");
             var peso = tdpeso.textContent;
 
@@ -15,18 +17,36 @@ var pacientes = document.querySelector(".subtitulo");
             var altura = tdaltura.textContent;
             console.log(altura);
 
+            // calcular imc
             var imc = peso/(altura*altura);
 
-            //acessa e altera o imc
-            var tdimc = paciente.querySelector(".info-imc");
-            tdimc.textContent = imc;
+            // variaveis com valor true
+            var pesoValido = true;
+            var alturaValida = true;  
+
+            if(pesoValido && alturaValida){
+                //acessa e altera o imc
+                var tdimc = paciente.querySelector(".info-imc");
+               tdimc.textContent = imc;
+                }
+
+            //define limites de peso altura
+            if(peso <= 0 || peso > 100){
+                console.log("peso invalido");
+                var pesoValido = false;
+                tdimc.textContent = "peso invalido!";
+    
+            }
+            if(altura <= 0 || altura> 300){
+                console.log("altura invalida");
+               var alturaValida = false;
+               tdimc.textContent = "altura invalida";
+            }
+            }
+
+           
             
-            if(peso< 0 || peso > 100){
-                alert("peso invalido");
-            }
-            if(altura < o || altura> 300){
-                alert("altura invalida");
-            }
+            
             
 
 
