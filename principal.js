b5e9f6aecf7fce0ca88c6152edbc35e81585fcea
rafakhjukjhikb <rafaela.castro.moura@escola.pr.gov.br>
2025-09-11 08:58:14 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,57 +1,48 @@
-            //muda o nome a partir do seletor de classe
-            var subtitulo= document.querySelector(".subtitulo");
-			var titulo = document.querySelector(".titulo");
-			titulo.textContent = "Rafa Nutrição";
-            subtitulo.textContent = "meus pacientes";
+//muda o nome a partir do seletor de classe
+var subtitulo = document.querySelector(".subtitulo");
+subtitulo.textContent = "Meus Pacientes";
+var Rafaela = document.querySelector(".titulo");
+Rafaela.textContent = "Rafaela Nutrição";
 
-            //acessara tag tr - paciente Paulo
-            var paciente = document.querySelector(".primeiro");
-           
-            for(var i = 0; i < paciente.length; i++){
-                 //seleciona o conteudo
-            var tdpeso =paciente.querySelector(".info-peso");
-            var peso = tdpeso.textContent;
+//acessar a tag tr - paciente Paulo
+var pacientes =document.querySelectorAll(".paciente");
 
-            //seleciona o conteudo da altura da tag
-            var tdaltura = paciente.querySelector(".info-altura");
-            var altura = tdaltura.textContent;
-            console.log(altura);
+for(var i = 0; i < pacientes.length; i++){
+ var paciente = pacientes[i];
 
-            // calcular imc
-            var imc = peso/(altura*altura);
+//seleciona o conteúdo
+var tdpeso = paciente.querySelector(".info-peso");
+var peso = tdpeso.textContent;
 
-            // variaveis com valor true
-            var pesoValido = true;
-            var alturaValida = true;  
+//seleciona o conteuo altura a tag
+var tdaltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent;
 
-            if(pesoValido && alturaValida){
-                //acessa e altera o imc
-                var tdimc = paciente.querySelector(".info-imc");
-               tdimc.textContent = imc;
-                }
+//calcula o imc
+var imc = peso / (altura * altura);
 
-            //define limites de peso altura
-            if(peso <= 0 || peso > 100){
-                console.log("peso invalido");
-                var pesoValido = false;
-                tdimc.textContent = "peso invalido!";
-    
-            }
-            if(altura <= 0 || altura> 300){
-                console.log("altura invalida");
-               var alturaValida = false;
-               tdimc.textContent = "altura invalida";
-            }
-            }
+//variáveis com valor true
+var pesoValido = true;
+var alturaValida = true; 
 
-           
+//define limites de peso e altura
+if(pesoValido && alturaValida){
+
+    //acessa e altera o imc          
+    var tdimc = paciente.querySelector(".info-imc");
+    tdimc.textContent = imc;
+}   
+if(peso <= 0 || peso >= 1000){
+    var pesoValido = false;
+    tdimc.textContent = "peso invalida";
+}
+if(altura <=0 || altura>= 3.00){
+    var alturaValida = false;
+tdimc.textContent = ("altura invalida");
+} 
+
+}
+
+
+
             
-            
-            
-
-
-
-
-
-
-    
