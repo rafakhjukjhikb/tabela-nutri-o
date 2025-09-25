72d0799f3bce5b08e745e3aca88c4ef09a75088a
rafakhjukjhikb <rafaela.castro.moura@escola.pr.gov.br>
2025-09-25 07:59:46 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -30,11 +30,12 @@ if(pesoValido && alturaValida){
 
     //acessa e altera o imc          
     var tdimc = paciente.querySelector(".info-imc");
-    tdimc.textContent = imc;
+    tdimc.textContent = imc.toFixed(2);
 }   
 if(peso <= 0 || peso >= 1000){
     var pesoValido = false;
     tdimc.textContent = "peso invalida";
+    paciente.style.backgroundColor="red";
 }
 if(altura <=0 || altura>= 3.00){
     var alturaValida = false;
